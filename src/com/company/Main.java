77c24ec2825597026933c5616751f8f535854847
Yubai Jiang <yubaijiang@Yubais-MacBook-Air.local>
2017-03-03 17:32:06 +1100

    can set plugboard now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -162,6 +162,13 @@ public class Main {
         System.out.println("Please enter reflector, selecting from " + reflectorNameList + ".");
         String reflector = selectRotor(reflectorSet, reflectorNames);
 
+        /*
+        Setup plug-board
+         */
+
+        System.out.println("Please enter plug-board setting, even number of alphabets only.");
+        String plugboard = insertPlugs(keyboard);
+
 
     }
 
@@ -172,9 +179,9 @@ public class Main {
     /**
      * Rotor setup: request user input, check input validity, then select rotor String.
      *
-     *
-     * @param partSets collection or rotors or reflectors
-     * @param nameSets colloection of names of rotors or reflectors
+     * @param partSets: collection or rotors or reflectors
+     * @param nameSets: colloection of names of rotors or reflectors
+     * @return rotorSelected: String of selected rotor/reflector details
      */
 
     public static String selectRotor(String[] partSets, String[] nameSets){
@@ -229,6 +236,7 @@ public class Main {
      * @param rotorSelected: detail(String) of the rotor selected by user
      * @param partSet: collection of parts
      * @param steppingPositionSet: collection of steppingPosition of all rotors
+     * @return steppingPositionLoaded: a string contains character of stepping position
      */
 
     public static String loadRotorSteppingPosition(String rotorSelected, String[] partSet, String[] steppingPositionSet){
@@ -249,6 +257,129 @@ public class Main {
         }
 
         return steppingPositionLoaded;
+
+    }
+
+    /**
+     * Function: Insert plugs
+     * scan user input and check validity
+     *
+     * @param scan: a scanner for user input
+     * @param alphabets: all alphabets
+     * @return insertedPlugs: String of user input
+     */
+
+    public static String insertPlugs(String alphabets){
+
+        Scanner scan = new Scanner(System.in);
+        String insertedPlugs = scan.nextLine().toUpperCase();
+        int plugSize = insertedPlugs.length();
+        int alphabetsSize = alphabets.length();
+
+        boolean validitySize = true;
+        boolean validityString = false;
+        boolean[] validityCharacter = new boolean[20];
+
+        boolean validity = false;
+
+        //check user input validity
+        while(!validity) {
+
+            //check if user entered input of correct size
+            validitySize = false; //set to default, change to true later in test.
+            while(!validitySize){
+
+                //check if the size is even and no more than 20
+                if((plugSize % 2 == 0) && plugSize <= 20){
+
+                    System.out.println(plugSize + " plugs provided.");
+                    validitySize = true;
+
+                } else {
+
+                    System.out.println("Input invalid, wrong number of characters. Please enter again.");
+                    insertedPlugs = scan.nextLine().toUpperCase(); // reset input
+                    plugSize = insertedPlugs.length(); // reset size
+
+                }
+
+            }
+
+            //check if the input are all alphabets
+            while (!validityString) {
+
+                //set all characters valid as default, test later
+
+                for(int i = 0; i < 20; i++){
+
+                    validityCharacter[i] = true;
+
+                }
+
+                //check if characters in input match to any alphabet.
+                for (int i = 0; i < plugSize; i++) {
+
+                    validityCharacter[i] = false; // default, change later.
+
+                    for (int j = 0; j < alphabetsSize; j++) {
+
+
+                        if (insertedPlugs.charAt(i) == (alphabets.charAt(j))) {
+
+                            validityCharacter[i] = true;
+
+                        }
+                    }
+
+                    if (validityCharacter[i]) {
+
+                        System.out.println(insertedPlugs.charAt(i) + " is a valid character.");
+
+                    } else {
+
+                        System.out.println(insertedPlugs.charAt(i) + " is an invalid character.");
+
+                    }
+                }
+
+                //assess matching results
+
+                validityString = true; //set as default. If false, change to false later
+                for (int i = 0; i < plugSize; i++) {
+
+                    if (!validityCharacter[i]) {
+
+                        validityString = false;
+                        validitySize = false;
+                        System.out.println("Input invalid, invalid characters. Please enter again.");
+                        insertedPlugs = scan.nextLine().toUpperCase(); // reset input
+                        plugSize = insertedPlugs.length(); // reset size
+
+                        break;
+
+                    }
+
+                }
+
+                if(!validityString){
+
+                    break;
+
+                }
+
+            }
+
+            //check if the input is valid (all alphabets, even, <=20)
+            if (validitySize && validityString) {
+
+                validity = true;
+                System.out.println("Letters " + insertedPlugs + " are plugged as pairs.");
+
+            }
+
+        }
+
+        return insertedPlugs;
 
     }
 }
